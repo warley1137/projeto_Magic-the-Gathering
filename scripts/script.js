32f9d9fd3d05171029f90daabf5906d13c146d4a
warley1137 <warleymendes1137@gmail.com>
2025-10-08 23:23:22 -0300

    :tada: Commit Inicial CSS

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+document.getElementById("Home").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+document.getElementById("Project").addEventListener("click", function () {
+  window.location.href = "project_01.html";
+});
+
+document.getElementById("Contact").addEventListener("click", function () {
+  window.location.href = "contact_01.html";
+});
+
+document.getElementById("Collections").addEventListener("click", function () {
+  window.location.href = "collections_01.html";
+});
+
+document.getElementById("Contact_Lucas").addEventListener("click", function () {
+  window.location.href = "https://github.com/Lucas-MMelo";
+});
